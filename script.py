c458563c212b5387e95fec8b5f7610a319123007
Florian Hauschild <florian.hauschild@fs.ei.tum.de>
2022-01-11 15:33:43 +0100

    rewrote plugin to use new abstraction class and use threaded backend

--- script.py
+++ script.py
@@ -1,62 +1,70 @@
 import xbmc
-import time
-import serial
+from lightcontroller import LightController
 
 class XBMCPlayer( xbmc.Player ):
 
     def __init__( self, *args ):
+        self.lightcontroller = LightController()
+        xbmc.log("Initialised LightController")
         pass
 
     def onPlayBackStarted( self ):
         # Will be called when xbmc starts playing a file
         xbmc.log( "LED Status: Playback Stopped, LED ON" )
-        ser.write("set mode 2\r")
-        ser.write("set step 1\r")
-        ser.write("set rgb 0 0 0\r")
-        xmbc.sleep(12)
-        ser.write("set atx off\r")
+        self.lightcontroller.abortprevious()
+        if self.lightcontroller.atx:
+            self.lightcontroller.mode = 2
+            self.lightcontroller.step = 1
+            self.lightcontroller.set_rgb( 0, 0, 0)
+            self.lightcontroller.set_rgb_limit( 2, 2, 2, 30, 30)
+            self.lightcontroller.set_lights( False, delay=20)
 
     def onPlayBackEnded( self ):
         # Will be called when xbmc stops playing a file
         xbmc.log( "LED Status: Playback Stopped, LED OFF" )
-        ser.write("set atx on\r")
-        ser.write("set mode 1\r")
-        ser.write("set step 4\r")
-        ser.write("set rgb 255 255 255\r")
+        self.lightcontroller.abortprevious()
+        if not self.lightcontroller.atx:
+            self.lightcontroller.set_lights(True)
+        self.lightcontroller.mode = 1
+        self.lightcontroller.step = 4
+        self.lightcontroller.set_rgb( 100, 100, 100)
+        self.lightcontroller.set_rgb( 255, 255, 255, delay=30)
 
     def onPlayBackStopped( self ):
         # Will be called when user stops xbmc playing a file
         xbmc.log( "LED Status: Playback Stopped, LED OFF" )
-        ser.write("set atx on\r")
-        ser.write("set mode 1\r")
-        ser.write("set step 4\r")
-        ser.write("set rgb 255 255 255\r")
+        self.lightcontroller.abortprevious()
+        if not self.lightcontroller.atx:
+            self.lightcontroller.set_lights(True)
+        self.lightcontroller.mode = 1
+        self.lightcontroller.step = 4
+        self.lightcontroller.set_rgb( 255, 255, 255)
 
     def onPlayBackPaused( self ):
         # Will be called when user Pauses xbmc playing a file
         xbmc.log( "LED Status: Playback Paused, LED ON BUT STILL" )
-        ser.write("set atx on\r")
-        ser.write("set mode 1\r")
-        ser.write("set step 2\r")
-        ser.write("set rgb 100 100 100\r")
+        self.lightcontroller.abortprevious()
+        if not self.lightcontroller.atx:
+            self.lightcontroller.set_lights(True)
+        self.lightcontroller.mode = 1
+        self.lightcontroller.step = 2
+        self.lightcontroller.set_rgb( 100, 100, 100)
 
     def onPlayBackResumed( self ):
         # Will be called when user stops xbmc playing a file
         xbmc.log( "LED Status: Playback Resumed, LED ON" )
-        ser.write("set mode 0\r")
-        ser.write("set rgb 0 0 0\r")
-        xmbc.sleep(10)
-        ser.write("set atx off\r")
+        self.lightcontroller.abortprevious()
+        if self.lightcontroller.atx:
+            self.lightcontroller.mode = 0
+            self.lightcontroller.set_rgb( 10, 10, 10)
+            self.lightcontroller.set_lights( False, delay=5)
 
 player = XBMCPlayer()
 xbmc.log("Started Lightcontroller")
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-ser.write("set rgb 255 0 0\n\r")
 xbmc.sleep(10)
-ser.write("set rgb 255 255 255\n\r")
-time.sleep(10)
 while(not xbmc.abortRequested):
     xbmc.sleep(100)
 
-ser.close()
+del player
+xbmc.sleep(10)
 xbmc.log( "LED Status: Script Stopped" )
